a92d01e5143992ae9a314b71554776986b2add33
tony2cssc <jijing.guo@gmail.com>
2016-05-15 16:00:29 +0800

    Init files for ubuntu-16.04-svr

---
+++ ubuntu-16.04-svr/bootstrap.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Change software source to China site
+sudo sed -i 's/http:\/\/a/http:\/\/cn.a/g' /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+
+# install necessary packages
+sudo apt-get install build-essential git
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+
+# install nodejs stable
+# nvm install stable
+ 
+# install meteor
+curl https://install.meteor.com/ | sh 
+
+# 
+# sudo apt-get install -y python-dev python-setuptools libjpeg-dev python-pip git
+# install below for taiga
+# sudo apt-get install libxml2-dev
+# sudo apt-get install libxslt1-dev 
+
+# sudo pip install virtualenv
+
+# cd ~
+# virtualenv oscar
+# virtualenv frob
+
+# cd /vagrant
+# git clone https://github.com/tangentlabs/django-oscar.git 
+# cd django-oscar
+# source ~/oscar/bin/activate
+# make sandbox
+# sites/sandbox/manage.py runserver 0.0.0.0:8000
+
+# Building your own shop
+# cd /vagrant
+# source ~/frob/bin/activate
+# pip install django-oscar
+# django-admin.py startproject frobshop
