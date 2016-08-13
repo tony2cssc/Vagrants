37bd5a17c181a13965a95ef11e72d136669ee43f
tony2cssc <jijing.guo@gmail.com>
2016-08-13 16:09:42 +0800

    did necessary modification

--- ubuntu-16.04-svr/bootstrap.sh
+++ ubuntu-16.04-svr/bootstrap.sh
@@ -3,25 +3,22 @@
 # Change software source to China site
 sudo sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/ftp.sjtu.edu.cn/g' /etc/apt/sources.list
 
-sudo apt update
-sudo apt dist-upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # install necessary packages
-sudo apt install build-essential -y
+sudo apt-get install build-essential -y
 
 # clean unnecessary files
-sudo apt autoremove && sudo apt clean
+sudo apt-get autoremove && sudo apt-get clean
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
-
-# install nodejs LTS
-nvm install --lts
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+# nvm install --lts
  
 # install meteor
 curl https://install.meteor.com/ | sh 
 
-# 
 # sudo apt-get install -y python-dev python-setuptools libjpeg-dev python-pip git
 # install below for taiga
 # sudo apt-get install libxml2-dev
